b079c0329f07939e872413d2fc14ccb080fbf4dc
Doug Schamberg <doug.schamberg@gmail.com>
2019-12-07 08:24:21 -0700

    Updated @proplay/utils to 0.0.2, removed unnecessary dependancies

--- users/src/index.ts
+++ users/src/index.ts
@@ -9,7 +9,7 @@ import { createConnection, useContainer } from 'typeorm';
 import { ApolloServer } from 'apollo-server';
 import { UserResolver, PostResolver } from './resolvers';
 import { resolveUserReference } from './referenceResolvers';
-import { service as broker } from './brokers';
+import { service as broker, user as userBroker } from './brokers';
 
 
 const port = process.env.APOLLO_SERVER_PORT || 3002
